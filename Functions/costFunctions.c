8ecc8e986ed9810bdf67abd5e9330a6fdec29f98
H1er <jesusgarciacarrasco99@gmail.com>
2020-05-06 14:51:42 +0200

    added basic cost function

---
+++ Functions/costFunctions.c
@@ -0,0 +1,14 @@
+#include "costFunctions.h"
+#include <math.h>
+
+
+void calculatecost()
+{
+
+}
+
+double cuadratic (double expected, double actual)
+{
+    return pow(expected-actual, 2);
+}
+
---
+++ Functions/costFunctions.h
@@ -0,0 +1,18 @@
+#ifndef COSTFUNCTIONS_H
+#define	COSTFUNCTIONS_H
+
+
+
+void calculatecost();
+
+double cuadratic (double expected, double actual);
+
+
+
+
+
+
+
+
+
+#endif /* COSTFUNCTIONS_H */
